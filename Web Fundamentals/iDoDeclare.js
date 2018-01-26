0e8a16dbdcf98b00b674c83588d76ecabb1af747
laurencesk <laurencesk@gmail.com>
2018-01-25 18:05:51 -0800

    Add files via upload

---
+++ Web Fundamentals/iDoDeclare.js
@@ -0,0 +1,29 @@
+var num1 = 5
+console.log(num1)
+
+var num2 = 100.5
+console.log(num2)
+
+var num3 = 0
+console.log(num3)
+
+var num4 = -20
+console.log(num4)
+
+var str1 = "Hellow World!"
+console.log(str1)
+
+var str2 = "My name is Laurence"
+console.log(str2)
+
+var str3 = "This is the third string"
+console.log(str3)
+
+var bool1 = true
+console.log(bool1)
+
+var bool2 = false
+console.log(bool2)
+
+var und = undefined
+console.log(und)
