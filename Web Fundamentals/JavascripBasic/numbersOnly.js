7cae6488e18daf82ef7fe8093e71702585632221
laurencesk <laurencesk@gmail.com>
2018-01-26 17:25:29 -0800

    Update numbersOnly.js

--- Web Fundamentals/JavascripBasic/numbersOnly.js
+++ Web Fundamentals/JavascripBasic/numbersOnly.js
@@ -7,3 +7,25 @@ function numbersOnly(arr){
     }
     return newArr;
 }
+
+
+function numbersOnlyInline(arr){
+    var count = 0;
+    for(var i=0; i < arr.length; i++){
+        if(typeof(arr[i]) !=="number"){
+            for(var j = i+1; j < arr.length; j++ ){
+                if(typeof(arr[j]) =='number'){
+                  temp = arr[j]
+                  arr[j] = arr[i]
+                  arr[i] = temp
+                  count++
+                  break
+                }
+            }
+        }
+    }
+    for(var i = 1; i<count; i++){
+      arr.pop()
+    }
+      return arr;
+}
