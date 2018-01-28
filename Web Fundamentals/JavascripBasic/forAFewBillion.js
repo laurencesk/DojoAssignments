16d2c560f1d3b6c8f774e10a9dc4adca5a519c86
laurencesk <laurencesk@gmail.com>
2018-01-27 18:38:04 -0800

    Update forAFewBillion.js

--- Web Fundamentals/JavascripBasic/forAFewBillion.js
+++ Web Fundamentals/JavascripBasic/forAFewBillion.js
@@ -12,3 +12,19 @@ for(var i =1; i<=30; i++){
   }
 }
 console.log(sum/100)
+
+
+Bonus
+
+var payment = 0
+var current = 1
+for(var i =1;payment<=10000;i++){
+    if (i==1) {
+      sum = 1
+    }
+    else {
+      current*=2
+      payment += current
+    }
+}
+console.log("i="+(i-1))
