1d4ce86783f4f281eda30f7cf35875a479487d91
laurencesk <laurencesk@gmail.com>
2018-02-04 15:26:02 -0800

    Update typeList.py

--- Python and Django/Python/typeList.py
+++ Python and Django/Python/typeList.py
@@ -2,14 +2,16 @@ l = ['magical unicorns',19,'hello',98.98,'world']
 
 sum = 0
 string =""
+datatype = 0
 
 for i in range(0,len(l)):
-    if type(l[i]) is int or type(l[2]) is float:
+    print i
+    if type(l[i]) is int or type(l[i]) is float:
         sum+=l[i]
 
     elif type(l[i]) is str:
         string+=l[i]
 
-print sum, str
+print sum, string
 
 # print type(l[1])
