1c13437238bc04eaea3945011c1c91969717c005
laurencesk <laurencesk@gmail.com>
2018-02-04 22:06:46 -0800

    Update typeList.py

--- Python and Django/Python/typeList.py
+++ Python and Django/Python/typeList.py
@@ -3,15 +3,30 @@ l = ['magical unicorns',19,'hello',98.98,'world']
 sum = 0
 string =""
 datatype = 0
+intCount = 0
+strCount = 0
+listLen = len(l)
 
 for i in range(0,len(l)):
-    print i
+
     if type(l[i]) is int or type(l[i]) is float:
         sum+=l[i]
+        intCount+=1
 
     elif type(l[i]) is str:
         string+=l[i]
+        strCount+=1
 
-print sum, string
+if listLen == intCount:
+    print "The list you entered is of integer type"
+    print "Sum = ",sum
 
-# print type(l[1])
+elif listLen == strCount:
+    print "The list you entered is of integer type"
+    print "String = ",string
+
+
+elif listLen != strCount or listLen != intCount:
+    print "The list you entered is of mixed type"
+    print "Sum = ",sum
+    print "String = ",string
