fc82056df99eed81f3ba356c374e0de97f8bc8d0
laurencesk <laurencesk@gmail.com>
2018-02-05 17:12:31 -0800

    Add files via upload

---
+++ Python and Django/Python/makingAndReadingFromDictionaries.py
@@ -0,0 +1,9 @@
+dic = {
+"name": "Laurence",
+"age": 29,
+"country of birth": "Taiwan",
+"favorite language": "Python"
+}
+
+for key, value in dic.items():
+    print "My {} is {}".format(key,value)
