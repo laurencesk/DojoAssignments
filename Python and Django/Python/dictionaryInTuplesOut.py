8fecd0150a0fc996043c996228a24b69f8ee9efe
laurencesk <laurencesk@gmail.com>
2018-02-05 18:15:36 -0800

    Add files via upload

---
+++ Python and Django/Python/dictionaryInTuplesOut.py
@@ -0,0 +1,11 @@
+my_dict = {
+  "Speros": "(555) 555-5555",
+  "Michael": "(999) 999-9999",
+  "Jay": "(777) 777-7777"
+}
+
+def dictionaryInTuplesOut(arr):
+    print arr.items()
+
+
+dictionaryInTuplesOut(my_dict)
