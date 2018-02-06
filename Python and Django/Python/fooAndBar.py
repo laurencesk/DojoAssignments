ba7d1b1a58b1b132b4f1fba6aea7bdce6b7294e5
laurencesk <laurencesk@gmail.com>
2018-02-05 22:20:37 -0800

    Add files via upload

---
+++ Python and Django/Python/fooAndBar.py
@@ -0,0 +1,9 @@
+for i in range(100,1000001):
+    count = 0
+    for j in range(1,i+1):
+        if i % j ==0:
+            count+=1
+    if count == 2 and i ** :
+        print str(i)+" is a bar"
+    else:
+        print str(i)+" is not bar"
