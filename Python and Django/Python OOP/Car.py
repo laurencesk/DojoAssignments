d55cbfe81ddaa609c2bab954cba9d9a258762a60
laurencesk <laurencesk@gmail.com>
2018-02-06 08:56:40 -0800

    Update Car.py

--- Python and Django/Python OOP/Car.py
+++ Python and Django/Python OOP/Car.py
@@ -11,6 +11,11 @@ class Car(object):
         else:
             return "12%"
     def display_all(self):
-        print self.price, self.speed,self.fuel,self.mileage,selft.tax()
+        print self.price, self.speed,self.fuel,self.mileage,self.tax()
 
 car1 = Car(2000,"35mpg","Full","15mpg")
+car2 = Car(2000,"5mpg","Not Full","105mpg")
+car3 = Car(2000,"15mpg","Kind of Full","95mpg")
+car4 = Car(2000,"25mpg","Full","25mpg")
+car5 = Car(2000,"45mpg","Empty","25mpg")
+car6 = Car(20000000,"35mpg","Empty","15mpg")
