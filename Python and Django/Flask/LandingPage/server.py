0c64ace924a896ce7fbef99be940ed1079fd2fb0
laurencesk <laurencesk@gmail.com>
2018-02-08 11:51:54 -0800

    Update server.py

--- Python and Django/Flask/LandingPage/server.py
+++ Python and Django/Flask/LandingPage/server.py
@@ -9,12 +9,23 @@ def index():
 def ninja():
     return render_template('ninja.html')
 
-@app.route('/dojos/new',methods=['GET','POST'])
+@app.route('/dojos/new')
 def form():
-    print "Got Post Info"
-    name = request.form['name']
+    # name = request.form['name']
     # email = request.form['email']
     # # print neme, email
-    return redirect('/')
+    # return redirect('/dojos/new.html')
+    return render_template('/dojos/new.html')
+
+@app.route('/users', methods=['POST'])
+def create_user():
+   print "Got Post Info"
+   # we'll talk about the following two lines after we learn a little more
+   # about forms
+   # name = request.form['name']
+   # email = request.form['email']
+   # redirects back to the '/' route
+   return redirect('/')
+
 
 app.run(debug = True)
